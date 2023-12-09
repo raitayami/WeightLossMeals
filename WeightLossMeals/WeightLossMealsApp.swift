8a1f8172b887105f393823d626142fe5f48b3b73
raitayami <66219215+raitayami@users.noreply.github.com>
2023-12-09 12:28:59 +0000

    Initial Commit

---
+++ WeightLossMeals/WeightLossMealsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeightLossMealsApp.swift
+//  WeightLossMeals
+//
+//  Created by Tayami Rai on 09/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeightLossMealsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
