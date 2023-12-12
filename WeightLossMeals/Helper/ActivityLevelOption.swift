16cf019ec80a02ba12f7b1f385f0a8c0d2317fa0
raitayami <66219215+raitayami@users.noreply.github.com>
2023-12-12 13:48:46 +0000

    - Create second detail view after user has completed first detail view - Refactor second detail view because code gets repeated

---
+++ WeightLossMeals/Helper/ActivityLevelOption.swift
@@ -0,0 +1,41 @@
+//
+//  ActivityLevelOption.swift
+//  WeightLossMeals
+//
+//  Created by Tayami Rai on 12/12/2023.
+//
+
+import Foundation
+import SwiftUI
+
+
+struct ActivityLevelOption{
+    
+    var title: String
+    var description: String
+    var backgroundColor = Color.blue
+    
+    func row() -> some View{
+        
+        ZStack{
+            VStack(alignment: .leading){
+                Text(title)
+                    .foregroundStyle(.white)
+                    .font(.title2)
+                    .bold()
+                    .padding([.top, .leading, .bottom], 10)
+                
+                Text(description)
+                    .padding([.leading, .bottom], 15)
+                    .font(.caption)
+                
+            }
+            
+        }
+        .frame(width: 350, alignment: .leading)
+        .background(backgroundColor)
+        .cornerRadius(10)
+        .shadow(color: .black, radius: 4, x: 0, y: 1)
+        .padding()
+    }
+}
--- WeightLossMeals/Views/Form/SecondDetailView.swift
+++ WeightLossMeals/Views/Form/SecondDetailView.swift
@@ -13,7 +13,43 @@ struct SecondDetailView: View {
     
     var body: some View {
         VStack{
-            Text("Wati")
+            Text("Activity Level")
+                .foregroundStyle(.mint)
+                .font(.title)
+                .bold()
+                .italic()
+            
+            ActivityLevelOption(
+                title: "Little/ no exercise",
+                description: "You know you should exercise but don't.",
+                backgroundColor: .gray
+            ).row()
+                
+            ActivityLevelOption(
+                title: "1-3 times a week",
+                description: "You make sure you go weekly. You try not too miss out",
+                backgroundColor: .pink
+            ).row()
+            
+            ActivityLevelOption(
+                title: "4-5 times a week",
+                description: "You have a plan. You want to be consistent. You have some goal hence why you go 4-5 times a week.",
+                backgroundColor: .blue
+            ).row()
+            
+            ActivityLevelOption(
+                title: "Daily or 3-4 times intense",
+                description: "You have a goal. The workouts are difficult. You are serious about your body.",
+                backgroundColor: .red
+            ).row()
+            
+            ActivityLevelOption(
+                title: "Intense 6-7 times a week",
+                description: "You are always active. The workouts are difficult. Your job is active too.",
+                backgroundColor: .green
+            ).row()
+            
+     
         }
     }
 }
